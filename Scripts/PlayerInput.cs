e4c3554cb47323fce8934c1633ce3203db2c9125
postall74 <postall_mcs@mail.ru>
2022-06-26 16:29:37 +0500

    Class Simplification PlayerInput
    Use InputGetAxisRaw vice GetKey

--- Scripts/PlayerInput.cs
+++ Scripts/PlayerInput.cs
@@ -5,42 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Player))]
 public class PlayerInput : MonoBehaviour
 {
-    private Player _player;
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
 
-    public float Direction { get; private set; }
+    public float DirectionHorizontal { get; private set; }
+    public float DirectionVertical { get; private set; }
     public bool Jump { get; private set; }
-
-    private void Start()
-    {
-        _player = GetComponent<Player>();
-    }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            Direction = 1f;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            Direction = -1f;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            Direction = 1f;
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            Direction = -1f;
-        }
-        else
-        {
-            Direction = 0f;
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-            Jump = true;
-        else
-            Jump = false;
+        DirectionHorizontal = Input.GetAxisRaw(Horizontal);
+        DirectionVertical = Input.GetAxisRaw(Vertical);
+        Jump = Input.GetKey(KeyCode.Space);
     }
 }
